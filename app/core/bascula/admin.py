8f17d3cd9c1931d95f97b03125966869759115bc
arnaldocorreaxO <arnaldo.guillermo@gmail.com>
2021-07-23 18:45:58 -0400

    Change Bascula

--- app/core/bascula/admin.py
+++ app/core/bascula/admin.py
@@ -45,7 +45,7 @@ class ClienteResource(resources.ModelResource):
 
 class ClienteAdmin(ImportExportModelAdmin):
     resource_class = ClienteResource
-    fields = ('codigo', 'denominacion',)
+    fields = ('codigo', 'denominacion','asociacion')
 
 ''' 
 =====================
